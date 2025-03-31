cf72517821dba24c75e768259251ff0096235bea
MarlockT <nunochenrique@gmail.com>
2025-03-31 09:20:06 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+var nomeInserido = prompt("Insira seu nome");
+
+alert ('Olá, ' + nomeInserido + ', seja bem-vindo!')
+
+var n1 = parseInt(prompt("Insira um número inteiro"));
+
+var n2 = parseInt(prompt('Insira outro número inteiro'))
+
+var soma = n1 + n2
+
+alert(`A soma de ${n1} com ${n2} é ${n1 + n2}`);
+
+
+console.log(`Soma = ${n1 + n2}`)
+console.log(`Mult = ${n1 * n2}`)
+console.log(`Sub = ${n1 - n2}`)
+console.log(`Div = ${n1 / n2}`)
